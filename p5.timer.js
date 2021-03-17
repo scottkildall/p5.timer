60813ad286ac2dd7aef455dfc493239c4aa59b12
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2021-03-16 21:48:05 -0700

    copied working p5.timer.js to all other folders

--- p5.timer.js
+++ p5.timer.js
@@ -1,29 +1,28 @@
 /*******************************************************************************************************************
 //
 //  Class: Timer
+//  for P5.js
 //
 //  Written by Scott Kildall
 //  Modified by Alina Xia
-//	for P5.js
-//
+//	
 //------------------------------------------------------------------------------------------------------------------
 // - Very simple but incredibly useful timer class
 // - Call start() whenever it expires to reset the time
 // - Call expired() to check to see if timer is still active
 //------------------------------------------------------------------------------------------------------------------
-// --> Additions: 
+//   Constructor: requires a timer duration, this can always be changed with setTimer()
+//------------------------------------------------------------------------------------------------------------------
+// --> Additions + Mods by Alina
 // new Timer(_duration, start) now creates a timer with an option to start immediately or not. default does not start. 
 // reset() covers the old start() functionality
 // pause() pauses the timer, allowing it to be restarted when needed. 
 // addTime(x) adds x millis to the remaining duration. it does not modify the original duration. 
 //  --> Can also use to subtract time w/ neg number
 // endTimer() forcibly ends the timer (will return true when expired() called); 
-//
 // --> Modifications:
 // start() -- now starts ONLY IF timer not currently running
 // --> starts if paused by pause() or timer expired
-//------------------------------------------------------------------------------------------------------------------
-//   Constructor: requires a timer duration, this can always be changed with setTimer()
 *********************************************************************************************************************/
 
 class Timer {
@@ -31,51 +30,63 @@ class Timer {
     constructor( _duration, start = false ) {
         this.startTime = millis();
         this.duration = _duration;
-        this.paused = !start; 
-        this.remainingDuration = _duration;
-    
+        this.paused = false;
+        this.pauseStartTime = 0;        // this will be millis() after the pause()
+        this.totalPauseTime = 0;        // so we can make accurate calculations
     }
 
 // MAIN FUNCTIONS
     //starts the timer if it was expired or paused
     start() { 
-        if ( this.expired() || this.paused ){
-            this.startTime = millis(); //restarts counting millis
-            if (this.expired()){ this.remainingDuration = this.duration; } //restarts
-            if (this.paused){ this.paused = false; } //unpauses
+        if( this.expired() ) {            
+            this.startTime = millis();      // restarts counting millis
+        }
+        else if( this.paused ) { 
+             // unpauses, add paused amount to total pause tume
+            this.totalPauseTime += millis() - this.pauseStartTime;
+            this.paused = false; 
         }
     }
 
     // returns true if the timer is expired, e.g. if millis() is greater than startTime + duration
     expired() {
-        return (this.startTime + this.remainingDuration) < millis();
+        return (this.startTime + this.duration + this.getPauseTime() ) < millis();
     }
 
-    //restarts timer regardless of status
+    // restarts timer regardless of status
     reset(){
-        this.startTime = millis(); 
+        this.startTime = millis();
+         this.totalPauseTime = 0;
+
+        if( this.paused ) {
+            this.pauseStartTime = this.startTime;
+        }
+        else {
+            this.pauseStartTime = 0;
+        }
     }
 
-    //pauses the timer 
+    // pauses the timer 
     pause(){
-        this.remainingDuration = this.getRemainingTime(); 
-        this.paused = true; 
+        if( this.paused === false ) {
+            this.pauseStartTime = millis();
+            this.paused = true; 
+        }
     }
     
-    //adds x millis to the remaining duration. 
+    // adds x millis to the remaining duration. 
     addTime(x){
-        this.remainingDuration + x; 
+        this.duration += x; 
     }
 
-    // set the duration, doesn't restart the timer
+    // set the duration, doesn't start the timer
     setTimer(_duration) {
         this.duration = _duration;
-        this.remainingDuration = _duration;
     }
     
     //forces an expired() state by setting remaining duration to zero. 
     endTimer(){
-        this.remainingDuration = 0; 
+        this.duration = 0; 
     }
 
 // SIMPLE ACCESSORS
@@ -86,19 +97,42 @@ class Timer {
 
     // returns remaining time in milliseconds, zero if timer is done
     getRemainingTime() {
-        if( this.expired() ){ return 0; }
-        return  (this.startTime + this.remainingDuration) - millis();
+        if( this.expired() ) {
+            return 0;
+        }
+        
+        // never return a neg number + account for pause time
+        let rt = this.startTime + this.duration + this.getPauseTime() - millis();
+        if( rt < 0 )
+            rt = 0;
+        return rt;
     }  
 
     // returns remaining % of timer, 0.0 through 1.0
     getPercentageRemaining() {
-        if( this.duration === 0 )  {  return 0.0; }// avoid div by zero error
-        if( this.expired() ) { return 0.0; }
+        if( this.duration === 0 )  { 
+            return 0.0;     // avoid div by zero error
+        }
+        if( this.expired() ) { 
+            return 0.0;
+        }
+
         return this.getRemainingTime()/this.duration;
     }
 
     // returns elapsed % of timer, 0.0 through 1.0
     getPercentageElapsed() {
         return 1.0 - this.getPercentageRemaining(); //refactor to remove duplication
-    }	 	
+    }	
+
+    // we use this to account for all paused actions: return active and past pause times
+    getPauseTime() {
+        let pauseTime = this.totalPauseTime;
+
+        if( this.paused ) {
+            pauseTime += (millis() - this.pauseStartTime);
+        }
+
+        return pauseTime;
+    } 	
 }
